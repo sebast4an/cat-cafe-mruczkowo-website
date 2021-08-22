d9fd378def806830466673f6f664e8aa8f409405
sebast4an <szczypinskisebastian@gmail.com>
2021-08-22 11:22:14 +0200

    final page information

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,9 @@ const importAll = r => r.keys().forEach(r);
 importAll(require.context('../assets/svg', true, /\.svg$/));
 importAll(require.context('../assets/images', true, /\.jpg$/));
 
+//js
+import './catsForAdoption';
+
 (() => {
   const mobileMenuButton = document.querySelector('.menu__hamburger-button');
   if (mobileMenuButton == null) return;
