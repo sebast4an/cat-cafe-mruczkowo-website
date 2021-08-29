9a8d258cf7d76a1670fb2c029f68c3e668620e61
sebast4an <szczypinskisebastian@gmail.com>
2021-08-29 19:23:50 +0200

    final mobile

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,26 @@ importAll(require.context('../assets/images', true, /\.jpg$/));
 import './catsForAdoption';
 
 (() => {
+  //navigation button
   const mobileMenuButton = document.querySelector('.menu__hamburger-button');
   if (mobileMenuButton == null) return;
 
   mobileMenuButton.addEventListener('click', e => {
     e.preventDefault();
     mobileMenuButton.classList.toggle('menu__hamburger-button--active');
+  });
+
+  //navigation background color
+  const navigation = document.querySelector('.navigation');
+  const startPage = document.querySelector('.start-page').offsetHeight;
+  const startPageHeight = startPage - startPage * 0.2;
+
+  window.addEventListener('scroll', () => {
+    if (scrollY > 10) navigation.classList.add('navigation--shadow');
+    if (scrollY > startPageHeight) navigation.classList.add('navigation--below-start-page');
+    if (scrollY === 0) {
+      navigation.classList.remove('navigation--below-start-page');
+      navigation.classList.remove('navigation--shadow');
+    }
   });
 })();
