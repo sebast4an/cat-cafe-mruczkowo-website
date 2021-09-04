496edf4d9214c02e0d97c194d2f6b2fb8c90684f
sebast4an <szczypinskisebastian@gmail.com>
2021-09-04 11:35:50 +0200

    refactor

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,10 +2,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const navigation = document.querySelector('.navigation');
   const navigationButton = document.querySelector('.menu__hamburger-button');
   const navigationList = document.querySelector('.links-list');
-  const startPage = document.querySelector('.start-page').offsetHeight;
-  const startPageHeight = startPage - startPage * 0.2;
 
-  let switchScrolling = 'disable'; //Default option
+  let switchScrolling = 'disable';
   const preventDefault = e => e.preventDefault();
 
   const changeScroll = option => {
@@ -26,19 +24,26 @@ window.addEventListener('DOMContentLoaded', () => {
     navigationButton.classList.toggle('menu__hamburger-button--active');
     navigationList.classList.toggle('links-list--active');
 
+    //changes navigation color to green when is black
     if (!navigation.classList.contains('navigation--below-start-page')) navigation.classList.add('navigation--below-start-page');
     else if (scrollY < 35) navigation.classList.remove('navigation--below-start-page');
 
+    //changes navigation logo to smal when is bigger
     if (scrollY > 35) return;
     else if (!navigation.classList.contains('navigation--shadow-and-scaling')) navigation.classList.add('navigation--shadow-and-scaling');
     else navigation.classList.remove('navigation--shadow-and-scaling');
+
+    //TODO: add event listener for navigation list links;
   });
 
   //change navigation while scrolling
   window.addEventListener('scroll', () => {
-    if (scrollY > 35) navigation.classList.add('navigation--shadow-and-scaling');
-    if (scrollY < 35) navigation.classList.remove('navigation--shadow-and-scaling');
+    //change navigation color moments before leaving start-page
+    const startPage = document.querySelector('.start-page').offsetHeight;
+    const startPageHeight = startPage - startPage * 0.2;
     if (scrollY > startPageHeight) navigation.classList.add('navigation--below-start-page');
     if (scrollY < startPageHeight) navigation.classList.remove('navigation--below-start-page');
+    if (scrollY > 35) navigation.classList.add('navigation--shadow-and-scaling');
+    if (scrollY < 35) navigation.classList.remove('navigation--shadow-and-scaling');
   });
 });
